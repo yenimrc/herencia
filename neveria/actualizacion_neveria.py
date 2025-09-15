afec660136368ba3e2da4548561c81619a6a1008
yeni mrc <yennialgc562425@gmail.com>
2025-09-15 13:31:06 -0600

    cambios realizados

--- neveria/actualizacion_neveria.py
+++ neveria/actualizacion_neveria.py
@@ -1,13 +1,21 @@
+from abc import ABC, abstractmethod
 
 # Clase base para todas las paletas
-class Paleta:
+class Paleta(ABC):
     def __init__(self,sabor:str,precio:float):
         #atributos de la clase base
         self.sabor=sabor
         self.precio=float(precio)
+
+    @abstractmethod
     def informacion(self):
-        #muestra la informacion basica de cada paleta
-        print(f'Sabor: {self.sabor}, precio: {self.precio:.2f} ')
+        #metodo que sera implentado por las subclases
+        pass
+
+    @abstractmethod
+    def descuento(self):
+        #metodo que sera implentado por lasubclases
+        pass
     
 
 class PaletaAgua(Paleta):
@@ -21,6 +29,10 @@ class PaletaAgua(Paleta):
         #sobrescribe el método de la clase base
         tipo = "con base de agua" if self.base_agua else "sin base de agua"
         print(f"Paleta de Agua - Sabor: {self.sabor}, Precio: ${self.precio:.2f} ({tipo})")
+    
+    def descuento(self,porcentaje):
+        #para calcular el descuento
+        return self.precio *(1-porcentaje/100)
 
 #clase derivada para paletas de crema
 class PaletaCrema(Paleta):
@@ -34,7 +46,10 @@ class PaletaCrema(Paleta):
         textura = "cremosa" if self.crema else "no cremosa"
         print(f"Paleta de Crema - Sabor: {self.sabor}, Precio: ${self.precio:.2f} ({textura})")   
 
-
+    def descuento(self,porcentaje):
+        #descuento pra paletas a base de crema
+        descuento_extra=0.05 if self.crema else 0
+        return self.precio * (1-porcentaje/100 + descuento_extra)
 
 #creamos la lista de paletas
 paletas = [
@@ -47,7 +62,18 @@ paletas = [
 ]
 
 print(' '*3,'Paletas disponibles')
+print(f"{'Tipo':<15} {'Sabor':<10} {'Precio':<8} {'Descuento 5%':<12} {'Base':<10}")
+
 for paleta in paletas:  
-    paleta.informacion()
+    #tipo de paleta
+    tipo='Agua' if isinstance(paleta, PaletaAgua) else 'A base de crema'
+    if isinstance(paleta,PaletaAgua):
+        informacion='Agua'
+    else:
+        informacion='Con crema' 
+    #para calcular descuento
+    descuento=paleta.descuento(5)
+    print(f"{tipo:<15} {paleta.sabor:<10} ${paleta.precio:<7.2f} ${descuento:<11.2f} {informacion:<10}")
+   
 
 
